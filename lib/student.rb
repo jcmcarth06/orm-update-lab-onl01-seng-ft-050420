e3de5fcbe984561b08ad6eb83de0dd0ef9d18915
Joshua McArthur <jcmcarth06@gmail.com>
2020-06-04 22:28:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,10 +48,15 @@ class Student
   def self.new_from_db
   end
 
-  def self.find_by_name
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Student.new(result[0], result[1], result[2])
   end
 
   def update
+    sql = "UPDATE students SET name = ?, album = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
   # Remember, you can access your database connection anywhere in this class
